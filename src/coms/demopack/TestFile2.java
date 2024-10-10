719c9660d7b96ead5298d1e875a2ab6fad64039b
Arumuru Venugopal <SLP11007@blrsimplilearn.com>
2024-10-10 15:17:17 +0530

    Merge Info Part-1

--- src/coms/demopack/TestFile2.java
+++ src/coms/demopack/TestFile2.java
@@ -5,5 +5,6 @@ public class TestFile2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Test file 2");
+		System.out.println("Another statement-1");
 	}
 }
