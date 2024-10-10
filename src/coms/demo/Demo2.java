3739d8da05623c5e756528191b527280ab48e7ec
Arumuru Venugopal <SLP11007@blrsimplilearn.com>
2024-10-10 12:02:34 +0530

    new file

---
+++ src/coms/demo/Demo2.java
@@ -0,0 +1,10 @@
+package coms.demo;
+
+public class Demo2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("this is another main method");
+	}
+
+}
