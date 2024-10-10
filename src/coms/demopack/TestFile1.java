1f7091adbccbbb1581b7542e5c4ff5636340a024
Arumuru Venugopal <SLP11007@blrsimplilearn.com>
2024-10-10 14:41:56 +0530

    demo pack files

---
+++ src/coms/demopack/TestFile1.java
@@ -0,0 +1,10 @@
+package coms.demopack;
+
+public class TestFile1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("demo pack file");
+	}
+
+}
